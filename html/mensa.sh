96cd50165cff6b9f6be1eee6e931a81fa0af175c
Heinz-Wilhelm Schäbe <pepsclena@gmx.de>
2016-10-27 22:26:03 +0200

    Speiseplan V1.0
    Sollte soweit klappen

--- html/mensa.sh
+++ html/mensa.sh
@@ -1,24 +1,33 @@
-#!bin/bash
-#Speiseplan in csv umwandeln und tagesaktuell in Dateien schreiben
-#HWS 15.10.16
+#!/bin/bash
+#Speiseplan in csv umwandeln und tagesaktuell in Dateien schreiben; mittag.txt und abend.txt werden von USB ausgelesen
+#HWS 15.10.16/26.10.16
 #INFO: in crontab packen und täglich abrufen
-date=`date +%w`
-if [ $date -eq 2 ]; then #montag
+date=`date +%u`
+if [ $date -eq 1 ]; then #montag
   #wget #Datei von HP TODO: Date mit Goe
-  #TODO: xls in xlsx umwandeln, bzw. eine andere Möglichkeit finden
+  #TODO: xls in xlsx umwandeln, bzw. eine andere Möglichkeit finden Viell. Webservice?
   xlsx2csv -d$ -s2 Speiseplan.xlsx > essen.csv
-  cut -d$ -f2 essen.csv > gesamt.txt
-  sed 's/^[ \t]*//' gesamt.txt -i #entfernt führende Leerzeichen
-  sed 's/ \+/ /g' gesamt.txt -i #entfernt überflüssige Leerzeichen
-  sed 's/$/<br>/' gesamt.txt -i
-  head gesamt.txt -n 3 > mittag.txt
-  tail gesamt.txt -n 1 > abend.txt
-else
-  dat=$(($date + 1))
-  cut -d$ -f$dat essen.csv > gesamt.txt
-  sed 's/^[ \t]*//' gesamt.txt -i #entfernt führende Leerzeichen
-  sed 's/ \+/ /g' gesamt.txt -i #entfernt überflüssige Leerzeichen
-  sed 's/$/<br>/' gesamt.txt -i
-  head gesamt.txt -n 3 > mittag.txt
-  tail gesamt.txt -n 1 > abend.txt
+  #INFO: date +%u
+  cut -d$ -f2 essen.csv > 1.txt
+  cut -d$ -f3 essen.csv > 2.txt
+  cut -d$ -f4 essen.csv > 3.txt
+  cut -d$ -f5 essen.csv > 4.txt
+  cut -d$ -f6 essen.csv > 5.txt
+  sed '1,1d' $date.txt -i #entfernt die erste Zeile
+  sed '1,1d' $date.txt -i #entfernt die erste Zeile
+  sed 's/^[ \t]*//' $date.txt -i #entfernt führende Leerzeichen
+  sed 's/ \+/ /g' $date.txt -i #entfernt überflüssige Leerzeichen
+  sed '/./!d' $date.txt -i #entfernt Leerzeilen
+  sed 's/$/<br>/' $date.txt -i
+  head $date.txt -n 3 > mittag.txt #Überprüfen
+  tail $date.txt -n 1 > abend.txt  #Überprüfen
+  #IDEA: Ergebnis als Mail an HWS zur Kontrolle und bei Bedarf Korrektur
+
+else #Di-Fr
+
+  sed 's/^[ \t]*//' $date.txt -i #entfernt führende Leerzeichen
+  sed 's/ \+/ /g' $date.txt -i #entfernt überflüssige Leerzeichen
+  sed 's/$/<br>/' $date.txt -i
+  head $date.txt -n 3 > mittag.txt #Überprüfen
+  tail $date.txt -n 1 > abend.txt #Überprüfen
 fi
