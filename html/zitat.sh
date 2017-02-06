37866cc19c9a7138cf8bd80009a07a62a88d0347
Heinz-Wilhelm Schäbe <pepsclena@gmx.de>
2017-02-06 08:38:26 +0100

    Zitat
    kleine fehler

--- html/zitat.sh
+++ html/zitat.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
+name="zitatdestages"
+a=0
 cd /home/pi/vplan/html
 curl -O http://www.zitate-online.de/zitatdestages.txt
-sed '$d' -i zitatdestages.txt
-sed '$d' -i zitatdestages.txt
+sed '$d' -i $name.txt
+sed '$d' -i $name.txt
+sed '1,1d' -i $name.txt
+# soweit... Sendung des Strings
+#cat $name.txt
+a="$(sed 's/[;&]/./g' $name.txt)"
+VAR="$(cat $name.txt)"
+echo $a
+API="$(cat pushbullet.txt)"
+MSG=$a
+#MSG="$(cat $name.txt)"
+DATE="$i"
+
+curl -u $API: https://api.pushbullet.com/v2/pushes -d type=note -d title="Zitat des Tages" -d body="$MSG"
+
+
 # TODO: Ausführung nachts via CronJob
