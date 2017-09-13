bc55b7b4c71f11c5f0ac497f7c513002f90ea40e
kpkpkp <git@c-roe.com>
2017-09-13 21:32:01 +0200

    Updated data entry form

--- html/form.php
+++ html/form.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 
 <!--html manifest="this.appcache"-->
-<html xmlns="http://www.w3.org/1999/html">
+<html xmlns="http://www.w3.org/1999/html" lang="de">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
