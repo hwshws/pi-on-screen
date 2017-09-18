752194a030ad252fab40625aefe49304d4e580d9
Unknown <tobias.k.99@web.de>
2017-09-18 18:09:25 +0200

    Fahrplan.php gelöscht
    unnötig

--- html/fahrplan.php
+++
@@ -1,4 +0,0 @@
-<img src="https://vrrf.finalrewind.org/Schelklingen/Schelklingen.png?frontend=png&backend=efa.DING" alt="Abfahrtstafel"
-     width="470" height="125">
-<!-- Notwendig für AJAX!
-<img src="https://vrrf.finalrewind.org/Schelklingen/Schelklingen.png?frontend=png&backend=efa.DING" id="reloader" onload="setTimeout('document.getElementById(\'reloader\').src=\'https://vrrf.finalrewind.org/Schelklingen/Schelklingen.png?frontend=png&backend=efa.DING', 5000)" /> -->
