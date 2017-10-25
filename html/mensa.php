d5a7344d7f660e1633f80f621d6cb43f1461f4bb
kpkpkp <git@c-roe.com>
2017-10-25 12:33:57 +0200

    Fixed an error in the sql query for mensa.php

--- html/mensa.php
+++ html/mensa.php
@@ -10,11 +10,9 @@ $pdo = new PDO('mysql:host=localhost;dbname=usb', $user, $pass);
 if (date("H") > "14") {
     $row = $pdo->query('SELECT Abend from mensa WHERE Datum = "' . $date . '" LIMIT 1;')->fetch();
     echo "<span style='font-weight: 600'>Abendessen:</span><br>";
-    //$abendessen = file_get_contents('abend.txt');
     echo $row[0];
 } else {
     $row = $pdo->query('SELECT Mittagessen from mensa WHERE Datum = "' . $date . '" LIMIT 1;')->fetch();
-    //$mittagessen = file_get_contents('mittag.txt');
     echo $row[0];
 }
 $pdo = null;
