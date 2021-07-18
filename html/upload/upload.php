42ed0e7d0b5d311b55c544c5433deea9181be7b0
Heinz <pepsclena@gmx.de>
2021-07-18 13:31:50 +0200

    Vegetarian Tuesday intercepted in the menu
    V. 1.8.

--- html/upload/upload.php
+++ html/upload/upload.php
@@ -12,26 +12,28 @@ if ($_POST['password'] == 'Achtopf') {
     require_once(__ROOT__ . '/Classes/PHPExcel.php');
     require_once(__ROOT__ . '/config.php');
     $filename = $_GET['filename'];
-    $target_dir = "/opt/usb/"; // TEST
-//$target_dir = "/homepages/38/d139650057/htdocs/";  //on Server
+    //$target_dir = "/opt/usb/"; // TEST
+    $target_dir = "/subdomains/comenius/httpdocs/usb/upload";  //on Server
     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
     $uploadOk = 1;
     $fileType = pathinfo($target_file, PATHINFO_EXTENSION);
     $target_name = $target_dir . $filename . "." . $fileType;
     if ($filename === null) {
         $uploadOk = 0;
+        echo "Zeile 22";
     }
     if ($filename != "Geburtstage" && $filename != "Speiseplan") {
         $uploadOk = 0;
+        echo "Zeile 26";
     }
 // Check file size
-    if ($_FILES["fileToUpload"]["size"] > 500000) {
-        //echo "Sorry, your file is too large.";
+    if ($_FILES["fileToUpload"]["size"] > 2000000) {
+        echo "Sorry, your file is too large.";
         $uploadOk = 0;
     }
 // Allow certain file formats
     if ($fileType != "xls" /*&& $fileType != "xlsx"*/) {
-        //echo "FALSCH!";
+        echo "Filetype FALSCH! 36";
         $uploadOk = 0;
     }
 // Check if $uploadOk is set to 0 by an error
@@ -49,7 +51,7 @@ if ($_POST['password'] == 'Achtopf') {
             echo "Update erfolgreich";
         }
     } else if ($uploadOk == 0 && $filename != "Text") {
-        echo "Sorry, da ging was schief!";
+        echo "Sorry, da ging was schief! Test und so.";
 // if everything is ok, try to parsing file
     } else {
         $objReader = new PHPExcel_Reader_Excel5();
@@ -67,8 +69,15 @@ if ($_POST['password'] == 'Achtopf') {
             for ($i = 0; $i < 5; $i++) {
                 $mittag = $objPHPExcel->getActiveSheet()->getCell($colums[$i] . '4')->getCalculatedValue();
                 $vegetarisch = $objPHPExcel->getActiveSheet()->getCell($colums[$i] . '5')->getCalculatedValue();
-                $nachtisch = $objPHPExcel->getActiveSheet()->getCell($colums[$i] . '6')->getCalculatedValue();//ex 7
-                $abend = $objPHPExcel->getActiveSheet()->getCell($colums[$i] . '10')->getCalculatedValue(); //ex 13
+                if ($vegetarisch === NULL) {
+                   $vegetarisch = $mittag;
+                   echo nl2br("Dienstag Vegi ".$vegetarisch."\n ");
+                   //echo "Dienstag";
+                   echo $mittag;
+                   //echo $vegetarisch;
+                }
+                $nachtisch = $objPHPExcel->getActiveSheet()->getCell($colums[$i] . '7')->getCalculatedValue();//ex 7
+                $abend = $objPHPExcel->getActiveSheet()->getCell($colums[$i] . '13')->getCalculatedValue(); //ex 13
                 //$essen[$date->format('d.m')] = array($mittag, $vegetarisch, $nachtisch, $abend);
                 //var_dump($objPHPExcel->getActiveSheet()->getCell($colums[$i] . '4')); echo '<br>';
                 $values = array(":Datum" => $date->format('d.m.Y'), ":Mittagessen" => $mittag,
