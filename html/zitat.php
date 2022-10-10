7b49beebbfb77705d76c47c73e08c3b5975081ee
Maximilian Klein <max.klein@visticle.de>
2022-10-10 20:45:37 +0200

    Re-Coded the whole FrontEnd

--- html/zitat.php
+++ html/zitat.php
@@ -1,8 +1,10 @@
 <?php echo file_get_contents('http://www.zitate-online.de/zitatdestages.txt'); ?>
 <script type="text/javascript">
-    let div = document.querySelector('#zitatdestages > strong > a');
-    try {
-        div.outerHTML = div.innerText;
-    } catch (ex) {}
+    let author = document.querySelector('#zitatdestages > strong > a').innerHTML;
     document.getElementById('zitatdestageslink').remove();
+    document.querySelector('#zitatdestages > strong').remove();
+    let message = document.getElementById('zitatdestages').innerHTML;
+    message = message.substring(2, message.length - 2);
+    message = message.replace(/<br>/g, '');
+    document.getElementById('zitat').innerHTML = '<p class="blockquote" style="font-size: 1.75rem !important;">' + message + '<br><span class="blockquote-footer">' + author + '</span></p>';
 </script>
